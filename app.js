b20b82dae2f3bc555d54c18fa4f8d8aca39f81da
BleepC <belentaywara@gmail.com>
2025-08-29 00:26:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ let numeroMaximo = 10;
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     console.log(numeroSecreto);
+    
     if (numeroDeUsuario === numeroSecreto){
-        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`)
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
         document.getElementById('reiniciar').removeAttribute('disabled');
 
     } else{
+        //El usuario no acertó
         if (numeroDeUsuario > numeroSecreto) {
            asignarTextoElemento('p', 'El número secreto es menor'); 
         } else {
@@ -34,11 +37,12 @@ function limpiarCaja() {
 function generarNumeroSecreto() {
     let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
     
+
     //Si ya sorteamos todos los numeros
     if (listaNumerosSorteados.length == numeroMaximo){
         asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles');
-        //Si el numero generado está incluido en la lista
     } else {
+        //Si el numero generado está incluido en la lista
         if (listaNumerosSorteados.includes(numeroGenerado)){
             return generarNumeroSecreto();
         } else{
@@ -54,6 +58,7 @@ function condicionesIniciales() {
     asignarTextoElemento('p', `Indica un número del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1; 
+    console.log(numeroSecreto);
 }
 
 function reiniciarJuego() {
@@ -67,6 +72,5 @@ function reiniciarJuego() {
     document.querySelector('#reiniciar').setAttribute('disabled', 'true');
 
 }
-
 
 condicionesIniciales();
